920c6219230488c0b9e8fc92f964549e106e0c58
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-04-16 20:02:57 -0300

    Configure Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
+
+  const config = new DocumentBuilder()
+    .setTitle('Universiteams API')
+    .setDescription(
+      'University-projects-manager: Pro Scientific Dissemination ',
+    )
+    .setVersion('1.0')
+    .addTag('universities')
+    .addTag('teams')
+    .addTag('research')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+
+  SwaggerModule.setup('api', app, document, customOptions);
+
   await app.listen(3000);
 }
 bootstrap();
