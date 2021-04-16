3cbea7a7a560400c1432cb55c9ad3391e121e85a
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-04-16 16:05:37 -0300

    Use it as the nestjs system logger excepting for first initialization logs
    Co-authored-by: NicoAntonelli <niconelli2@gmail.com>
    Co-authored-by:  JAcciarri  <acciarrijoshua@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
-import { AppLogger } from './logger/app-logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useLogger(new AppLogger());
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
