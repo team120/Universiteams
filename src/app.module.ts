37d62338f8f754de87c806142ac16c20a6829a73
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-04-16 16:05:23 -0300

    Configure nestjs-pino logger in app module
    Co-authored-by: NicoAntonelli <niconelli2@gmail.com>
    Co-authored-by:  JAcciarri  <acciarrijoshua@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,33 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectModule } from './project/project.module';
 import { UserModule } from './user/user.module';
 import { SharedModule } from './shared/shared.module';
-import { LoggerModule } from './logger/logger.module';
 import { ExceptionsModule } from './exceptions/exceptions.module';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ cache: true }),
     TypeOrmModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+        formatters: {
+          level: (level: string) => new Object({ level: level }),
+        },
+        prettyPrint:
+          process.env.NODE_ENV !== 'production'
+            ? {
+                colorize: true,
+                levelFirst: true,
+                translateTime: 'mm/dd/yyyy h:MM:ss TT Z',
+              }
+            : undefined,
+      },
+    }),
     ProjectModule,
     UserModule,
     SharedModule,
-    LoggerModule,
     ExceptionsModule,
   ],
   controllers: [AppController, AuthController, UniversityController],
