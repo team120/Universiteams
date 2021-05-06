973a5d53fe6c760ac32817e895d9cc6f789ba3af
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-05-06 13:05:55 -0300

    Take into account facility relations in project, user and institution queries

--- src/institution/institution.service.ts
+++ src/institution/institution.service.ts
@@ -19,7 +19,7 @@ export class InstitutionService {
   async findAll(): Promise<InstitutionShowDto[]> {
     this.logger.debug('Find all universities and their related departments');
     const universities = await this.institutionRepository
-      .find({ relations: ['researchDepartments'] })
+      .find({ relations: ['facilities', 'facilities.researchDepartments'] })
       .catch((err: Error) => {
         throw new DbException(err.message, err.stack);
       });
