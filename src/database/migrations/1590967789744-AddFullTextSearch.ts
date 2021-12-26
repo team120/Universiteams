69b05d571505cb8c2294bd7c91a44d33df0d26ca
Alejandro Recalde <alejandrorecalde5@gmail.com>
2021-12-26 18:45:26 -0300

    Try using trigger for ts_vector update

--- src/database/migrations/1590967789744-AddFullTextSearch.ts
+++ src/database/migrations/1590967789744-AddFullTextSearch.ts
@@ -39,14 +39,59 @@ export class AddFullTextSeach1590967789744 implements MigrationInterface {
         f.id,
         inst.id;
 
-        UPDATE project as p
-        SET document_with_weights = pwrd.document_with_weights
-        FROM project_with_related_data pwrd
-        WHERE p.id = pwrd.id;
+      UPDATE project as p
+      SET document_with_weights = pwrd.document_with_weights
+      FROM project_with_related_data pwrd
+      WHERE p.id = pwrd.id;
 
-        CREATE INDEX IF NOT EXISTS document_with_weights_idx
-        ON project
-        USING GIN(document_with_weights);
+      CREATE INDEX IF NOT EXISTS document_with_weights_idx
+      ON project
+      USING GIN(document_with_weights);
+
+      CREATE OR REPLACE FUNCTION set_tsvector() RETURNS trigger AS $$
+      BEGIN
+        new.document_with_weights := (
+          SELECT
+            setweight(to_tsvector('simple' ,coalesce(new.name, '')), 'A') || 
+            setweight(to_tsvector('simple' ,coalesce(new.type, '')), 'A') ||
+            setweight(to_tsvector('simple' ,coalesce(rd.name, '')), 'B') ||
+            setweight(to_tsvector('simple' ,coalesce(rd.abbreviation, '')), 'B') ||
+            setweight(to_tsvector('simple' ,coalesce(f.name, '')), 'B') ||
+            setweight(to_tsvector('simple' ,coalesce(f.abbreviation, '')), 'B') ||
+            setweight(to_tsvector('simple' ,coalesce(inst.name, '')), 'B') ||
+            setweight(to_tsvector('simple' ,coalesce(inst.abbreviation, '')), 'B') ||
+            setweight(to_tsvector('simple' ,coalesce(string_agg(usr.name || ' ' || usr."lastName", ' '), '')), 'C') ||
+            setweight(to_tsvector('simple' ,coalesce(string_agg(inter.name, ' '), '')), 'C') 
+          FROM project
+          INNER JOIN research_department rd
+            ON new."researchDepartmentId" = rd.id
+          INNER JOIN facility f
+            ON rd."facilityId" = f.id
+          INNER JOIN institution inst
+            ON f."institutionId" = inst.id
+          INNER JOIN interest_projects_project ip
+            ON ip."projectId" = new.id
+          INNER JOIN interest inter
+            ON ip."interestId" = inter.id
+          INNER JOIN enrollment enr
+            ON new.id = enr."projectId"
+          INNER JOIN "user" usr
+            ON enr."userId" = usr.id
+          GROUP BY 
+            new.id,
+            rd.id,
+            f.id,
+            inst.id
+          LIMIT 1
+        );
+        return new;
+      END
+      $$ LANGUAGE plpgsql;
+
+      CREATE TRIGGER update_tsvector 
+      AFTER INSERT OR UPDATE ON project
+      FOR EACH ROW
+      EXECUTE PROCEDURE set_tsvector();
     `);
   }
 
