6926a23d382d99d4f371e55a74167eb8d3b2de50
ale <alejandrorecalde5@gmail.com>
2022-01-09 20:58:15 -0300

    Remove root url from swagger endpoint list

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get('')
   @Redirect('api')
+  @ApiExcludeEndpoint()
   redirectToSwaggerDoc() {
     return 'Redirecting to universiteams api docs';
   }
