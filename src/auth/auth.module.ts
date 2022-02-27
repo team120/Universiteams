ea86bdf7d96708bdca67e5aeb3c5d4e5b2fa1143
Alejandro Recalde <alejandrorecalde5@gmail.com>
2022-02-27 20:53:48 -0300

    Set access and refresh token expiration times in auth module di container

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,10 @@ import { User } from '../user/user.entity';
 import { SerializationModule } from '../utils/serialization/serialization.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { IsAuthService } from './is-auth.service';
 import { IsAuthGuard } from './is-auth.guard';
+import { TokenExpirationTimes } from './token-expiration-times';
+import { TokenService } from './token.service';
 
 @Module({
   imports: [
@@ -14,12 +17,29 @@ import { IsAuthGuard } from './is-auth.guard';
     ConfigModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, IsAuthGuard],
-  exports: [
+  providers: [
+    AuthService,
+    IsAuthService,
     IsAuthGuard,
+    TokenService,
+    {
+      provide: TokenExpirationTimes,
+      useValue: new TokenExpirationTimes({
+        accessTokenExpiration: {
+          value: 15,
+          dimension: 'minutes',
+        },
+        refreshTokenExpiration: { value: 7, dimension: 'days' },
+      }),
+    },
+  ],
+  exports: [
     TypeOrmModule.forFeature([User]),
     SerializationModule,
     ConfigModule,
+    IsAuthService,
+    IsAuthGuard,
+    TokenService,
   ],
 })
 export class AuthModule {}
