faf438f5cc9e146c6fadb3a3adcce5732f3e0701
Alejandro Recalde <alejandrorecalde5@gmail.com>
2022-03-26 16:05:48 -0300

    Set different env files for dev and prod stages, since cors host is not the same in both

--- create-env-file.sh
+++ create-env-file.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-# TODO: Take any sensible secret as script arguments
-
-ENV_FILE='docker.env'
-
-if [[ -f "$ENV_FILE" ]]
-then
-    echo "$ENV_FILE already exists"
-    exit 1
-fi
 
 for ARGUMENT in "$@"
 do
@@ -19,6 +10,20 @@ do
     echo "$KEY=$VALUE"
     export "$KEY"="$VALUE"
 done
+
+if [ $ENV == 'dev' ]
+then
+    ENV_FILE='docker.dev.env'
+else
+    ENV_FILE='docker.prod.env'
+fi
+
+
+if [[ -f "$ENV_FILE" ]]
+then
+    echo "$ENV_FILE already exists"
+    exit 1
+fi
 
 touch $ENV_FILE
 echo POSTGRES_HOST="postgres" >> $ENV_FILE
@@ -36,4 +41,10 @@ echo EMAIL_USER=$EMAIL_USER >> $ENV_FILE
 echo EMAIL_CONFIRMATION_URL="http://localhost:5000/account/verify" >> $ENV_FILE
 echo SENDGRID_API_KEY=$SENDGRID_API_KEY >> $ENV_FILE
 echo SENDINBLUE_API_KEY=$SENDINBLUE_API_KEY >> $ENV_FILE
-echo FRONTEND_HOST="http://localhost:3000/" >> $ENV_FILE
+
+if [ $ENV = "dev" ]
+then
+    echo FRONTEND_HOST="http://localhost:3000" >> $ENV_FILE
+else
+    echo FRONTEND_HOST="TO_BE_DEFINED" >> $ENV_FILE
+fi
