9f999d4283c52eed3391d2c10913171dba04c3dd
Alejandro Recalde <alejandrorecalde5@gmail.com>
2023-07-23 13:27:56 -0300

    Redefine FRONTEND_HOST and add SAME_SITE_POLICY+SECURE_COOKIE to create env files scripts

--- create-test-env-file.sh
+++ create-test-env-file.sh
@@ -9,6 +9,8 @@ then
     exit 1
 fi
 
+FRONTEND_HOST="http://localhost:3000"
+
 touch $ENV_FILE
 echo POSTGRES_HOST="postgrestest" >> $ENV_FILE
 echo POSTGRES_PORT=5432 >> $ENV_FILE
@@ -21,6 +23,8 @@ echo ACCESS_TOKEN_SECRET="access_token_test_secret" >> $ENV_FILE
 echo REFRESH_TOKEN_SECRET="refresh_token_test_secret" >> $ENV_FILE
 echo EMAIL_VERIFICATION_LINK_SECRET="verification_link_secret" >> $ENV_FILE
 echo FORGET_PASSWORD_VERIFICATION_LINK_SECRET="forget_password_verification_link_secret" >> $ENV_FILE
-echo FORGET_PASSWORD_URL="http://localhost:5000/account/reset-password" >> $ENV_FILE
+echo FORGET_PASSWORD_URL="$FRONTEND_HOST/account/reset-password" >> $ENV_FILE
 echo EMAIL_USER="email@example.com" >> $ENV_FILE
-echo EMAIL_CONFIRMATION_URL="http://localhost:5000/account/verify" >> $ENV_FILE
+echo EMAIL_CONFIRMATION_URL="$FRONTEND_HOST/account/verify" >> $ENV_FILE
+echo SAME_SITE_POLICY="none" >> $ENV_FILE
+echo SECURE_COOKIE="true" >> $ENV_FILE
