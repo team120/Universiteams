6dac3dddf15229ebec910a9495aa0239d9bee294
JAcciarri <jacciarri@frro.utn.edu.ar>
2024-08-07 00:32:13 -0300

    feat: Create, Delete, Update Institutions

--- src/institution/institution.controller.ts
+++ src/institution/institution.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { InstitutionService } from './institution.service';
 import { InstitutionFindDto } from './dtos/institution.find.dto';
 import { AppValidationPipe } from '../utils/validation.pipe';
+import { InstitutionCreateDto } from './dtos/institution.create.dto';
 
 @ApiTags('institutions')
 @Controller('institutions')
@@ -12,5 +23,20 @@ export class InstitutionController {
   @Get()
   async get(@Query(AppValidationPipe) findOptions: InstitutionFindDto) {
     return this.institutionService.find(findOptions);
+  }
+  @Post()
+  async create(@Body() institution: InstitutionCreateDto) {
+    return this.institutionService.create(institution);
+  }
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.institutionService.delete(id);
+  }
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() institution: InstitutionCreateDto,
+  ) {
+    return this.institutionService.update(id, institution);
   }
 }
--- src/institution/institution.service.ts
+++ src/institution/institution.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PinoLogger } from 'nestjs-pino';
 import { Repository } from 'typeorm';
-import { DbException } from '../utils/exceptions/exceptions';
+import { DbException, NotFound } from '../utils/exceptions/exceptions';
 import { EntityMapperService } from '../utils/serialization/entity-mapper.service';
 import { InstitutionShowDto } from './dtos/institution.show.dto';
 import { Institution } from './institution.entity';
 import { InstitutionFindDto } from './dtos/institution.find.dto';
+import { InstitutionCreateDto } from './dtos/institution.create.dto';
+
+const institutionNotFoundError = new NotFound(
+  'El ID no coincide con ninguna institución',
+);
 
 @Injectable()
 export class InstitutionService {
@@ -32,5 +37,41 @@ export class InstitutionService {
       });
     this.logger.debug('Map universities to dto');
     return this.entityMapper.mapArray(InstitutionShowDto, universities);
+  }
+
+  async create(createDto: InstitutionCreateDto): Promise<InstitutionShowDto> {
+    this.logger.debug('Create a new institution');
+    const university = this.entityMapper.mapValue(Institution, createDto);
+    const createdUniversity = await this.institutionRepository
+      .save(university)
+      .catch((err: Error) => {
+        throw new DbException(err.message, err.stack);
+      });
+    this.logger.debug('Map university to dto');
+    return this.entityMapper.mapValue(InstitutionShowDto, createdUniversity);
+  }
+
+  async delete(institutionId: number): Promise<void> {
+    this.logger.debug('Delete an institution');
+    const institution = await this.institutionRepository.findOne({
+      where: { id: institutionId },
+    });
+    if (!institution) throw institutionNotFoundError;
+    await this.institutionRepository
+      .delete(institutionId)
+      .catch((err: Error) => {
+        throw new DbException(err.message, err.stack);
+      });
+    this.logger.debug(`Institution #${institution.id} successfully deleted`);
+  }
+
+  async update(institutionId: number, institutionDto: InstitutionCreateDto) {
+    this.logger.debug('Update an institution');
+    const institution = await this.institutionRepository.findOne({
+      where: { id: institutionId },
+    });
+    if (!institution) throw institutionNotFoundError;
+    await this.institutionRepository.update(institutionId, institutionDto);
+    this.logger.debug('Map university to dto');
   }
 }
