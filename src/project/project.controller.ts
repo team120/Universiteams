d764788510feaae370231eecf83fbdba75922f5c
NicoAntonelli <niconelli2@gmail.com>
2024-08-13 00:54:19 -0300

    Projects: controller endpoints for create, delete, update & updateState

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -24,6 +24,9 @@ import { EnrollmentRequestDto } from '../enrollment/dtos/enrollment.request.dto'
 import { UnenrollDto } from '../enrollment/dtos/unenroll.dto';
 import { EnrollmentRequestAdminDto } from '../enrollment/dtos/enrollment-request-admin.dto';
 import { EnrollmentChangeRole } from '../enrollment/dtos/enrollment-change-role';
+import { ProjectCreateDto } from './dtos/project.create.dto';
+import { ProjectUpdateDto } from './dtos/project.update.dto';
+import { ProjectUpdateStateDto } from './dtos/project.updateState.dto';
 
 @ApiTags('projects')
 @Controller('projects')
@@ -46,6 +49,40 @@ export class ProjectController {
     @Param('id', ParseIntPipe) projectId: number,
   ) {
     return this.projectService.findOne(projectId, request.currentUser);
+  }
+
+  @UseGuards(...IsEmailVerifiedGuard)
+  @ApiCookieAuth()
+  @Post()
+  async create(@Body() project: ProjectCreateDto) {
+    return this.projectService.create(project);
+  }
+
+  @UseGuards(...IsEmailVerifiedGuard)
+  @ApiCookieAuth()
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    return this.projectService.delete(id);
+  }
+
+  @UseGuards(...IsEmailVerifiedGuard)
+  @ApiCookieAuth()
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() project: ProjectUpdateDto,
+  ) {
+    return this.projectService.update(id, project);
+  }
+
+  @UseGuards(...IsEmailVerifiedGuard)
+  @ApiCookieAuth()
+  @Put(':id/update-state')
+  async updateState(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() project: ProjectUpdateStateDto,
+  ) {
+    return this.projectService.updateState(id, project);
   }
 
   @UseGuards(...IsEmailVerifiedGuard)
