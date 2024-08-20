b048c5f287035c1f157f2bb5ae8c1c354fb698b2
JAcciarri <jacciarri@frro.utn.edu.ar>
2024-08-20 11:57:32 -0300

    Removing from Interest entity: reference counters as no longer needed.

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -103,32 +103,6 @@ export class Seed {
     `);
   }
 
-  private async computeInterestsRefsCount() {
-    await this.projectRepo.query(`
-      UPDATE interest
-      SET "projectRefsCounter" = projects_interest_count."count"
-      FROM (
-        SELECT i.id, count(pi."interestId") as "count"
-        FROM public.interest i
-        LEFT JOIN project_interest pi
-          ON id = pi."interestId"
-        GROUP BY i.id
-        ) as projects_interest_count
-      WHERE interest.id = projects_interest_count.id;
-
-      UPDATE interest
-      SET "userRefsCounter" = users_interest_count."count"
-      FROM (
-        SELECT i.id, count(ui."interestId") as "count"
-        FROM public.interest i
-        LEFT JOIN user_interest ui
-          ON i.id = ui."interestId"
-        GROUP BY i.id
-        ) as users_interest_count
-      WHERE interest.id = users_interest_count.id;
-    `);
-  }
-
   private institutionsFactory() {
     const idGen = new NumUnitaryIncrease(1);
     return {
